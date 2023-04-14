d21f492d062b174915a612f4ee5def8e5f6941f4
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2023-04-14 16:22:53 -0400

    Finished the tutorial and things are kinda working

--- main.go
+++ main.go
@@ -8,13 +8,29 @@ import (
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		// Create an AWS resource (S3 Bucket)
-		bucket, err := s3.NewBucket(ctx, "new-test-bucket", nil)
+		bucket, err := s3.NewBucket(ctx, "new-test-bucket", &s3.BucketArgs{
+			Website: s3.BucketWebsiteArgs{
+				IndexDocument: pulumi.String("index.html"),
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		_, err = s3.NewBucketObject(ctx, "index.html", &s3.BucketObjectArgs{
+			Acl:         pulumi.String("public-read"),
+			ContentType: pulumi.String("text/html"),
+			Bucket:      bucket.ID(),
+			Source:      pulumi.NewFileAsset("index.html"),
+		})
 		if err != nil {
 			return err
 		}
 
 		// Export the name of the bucket
 		ctx.Export("bucketName", bucket.ID())
+
+		ctx.Export("bucketEndpoint", pulumi.Sprintf("http://%s", bucket.WebsiteEndpoint))
 		return nil
 	})
 }
