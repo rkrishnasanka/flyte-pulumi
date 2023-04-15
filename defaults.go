ab5244b1f40fe5d28c04826095c6a14295781086
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2023-04-15 14:54:15 -0400

    Updated the defaults using the input from the community.

--- defaults.go
+++ defaults.go
@@ -1,26 +1,28 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/ec2"
 	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/rds"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 // Find default VPC for the EKS cluster
-func findDefaultVPC(ctx *pulumi.Context) *ec2.LookupVpcResult {
+func findDefaultVPC(ctx *pulumi.Context) (*ec2.LookupVpcResult, error) {
 
 	// Find default VPC
 	vpcID := "vpc-009f4986f9d95a645"
 	vpc, err := ec2.LookupVpc(ctx, &ec2.LookupVpcArgs{Id: &vpcID})
 	if err != nil {
-		panic("Could not find VPC")
+		return nil, err
 	}
 
-	return vpc
+	return vpc, nil
 }
 
 // Find the default Relational Database Service (RDS)
-func findDefaultRDS(ctx *pulumi.Context) *rds.Instance {
+func findDefaultRDS(ctx *pulumi.Context) (*rds.LookupInstanceResult, error) {
 	// // Find default RDS
 	// rdsName := "Whatever"
 	// rdsID := "rds-1234567890"
@@ -29,7 +31,11 @@ func findDefaultRDS(ctx *pulumi.Context) *rds.Instance {
 	// if err != nil {
 	// 	panic("Could not find RDS")
 	// }
-
-	// return rds
-	panic("Not implemented")
+	instanceLookup, err := rds.LookupInstance(ctx, &rds.LookupInstanceArgs{
+		DbInstanceIdentifier: "database-1",
+	})
+	if err != nil {
+		return nil, fmt.Errorf("could not find RDS: %w", err)
+	}
+	return instanceLookup, nil
 }
