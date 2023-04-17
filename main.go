93b767e1c0a84867ec4c2b75d82dce39e7de0aef
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2023-04-17 18:18:22 -0400

    Updated the code for the baseline

--- main.go
+++ main.go
@@ -15,6 +15,8 @@ type FlyteConfig struct {
 	EksClusterName string
 	// The name of the EKS node group to create
 	PrimaryNodeGroupScalingConfig *ScalingConfig
+	// RDS password
+	RdsPassword string
 }
 
 type ScalingConfig struct {
@@ -105,79 +107,14 @@ func main() {
 			panic(err)
 		}
 
+		// // Create the RDS instance
+		// _, err = createRDSCluster(ctx, subnets, eksCluster)
+		// if err != nil {
+		// 	panic(err)
+		// }
+
 		return nil
 	})
-
-	// 	namespace, err := corev1.NewNamespace(ctx, "app-ns", &corev1.NamespaceArgs{
-	// 		Metadata: &metav1.ObjectMetaArgs{
-	// 			Name: pulumi.String("joe-duffy"),
-	// 		},
-	// 	}, pulumi.Provider(k8sProvider))
-	// 	if err != nil {
-	// 		return err
-	// 	}
-
-	// 	appLabels := pulumi.StringMap{
-	// 		"app": pulumi.String("iac-workshop"),
-	// 	}
-	// 	_, err = appsv1.NewDeployment(ctx, "app-dep", &appsv1.DeploymentArgs{
-	// 		Metadata: &metav1.ObjectMetaArgs{
-	// 			Namespace: namespace.Metadata.Elem().Name(),
-	// 		},
-	// 		Spec: appsv1.DeploymentSpecArgs{
-	// 			Selector: &metav1.LabelSelectorArgs{
-	// 				MatchLabels: appLabels,
-	// 			},
-	// 			Replicas: pulumi.Int(3),
-	// 			Template: &corev1.PodTemplateSpecArgs{
-	// 				Metadata: &metav1.ObjectMetaArgs{
-	// 					Labels: appLabels,
-	// 				},
-	// 				Spec: &corev1.PodSpecArgs{
-	// 					Containers: corev1.ContainerArray{
-	// 						corev1.ContainerArgs{
-	// 							Name:  pulumi.String("iac-workshop"),
-	// 							Image: pulumi.String("jocatalin/kubernetes-bootcamp:v2"),
-	// 						},
-	// 					},
-	// 				},
-	// 			},
-	// 		},
-	// 	}, pulumi.Provider(k8sProvider))
-	// 	if err != nil {
-	// 		return err
-	// 	}
-
-	// 	service, err := corev1.NewService(ctx, "app-service", &corev1.ServiceArgs{
-	// 		Metadata: &metav1.ObjectMetaArgs{
-	// 			Namespace: namespace.Metadata.Elem().Name(),
-	// 			Labels:    appLabels,
-	// 		},
-	// 		Spec: &corev1.ServiceSpecArgs{
-	// 			Ports: corev1.ServicePortArray{
-	// 				corev1.ServicePortArgs{
-	// 					Port:       pulumi.Int(80),
-	// 					TargetPort: pulumi.Int(8080),
-	// 				},
-	// 			},
-	// 			Selector: appLabels,
-	// 			Type:     pulumi.String("LoadBalancer"),
-	// 		},
-	// 	}, pulumi.Provider(k8sProvider))
-	// 	if err != nil {
-	// 		return err
-	// 	}
-
-	// 	ctx.Export("url", service.Status.ApplyT(func(status *corev1.ServiceStatus) *string {
-	// 		ingress := status.LoadBalancer.Ingress[0]
-	// 		if ingress.Hostname != nil {
-	// 			return ingress.Hostname
-	// 		}
-	// 		return ingress.Ip
-	// 	}))
-
-	// 	return nil
-	// })
 
 }
 
